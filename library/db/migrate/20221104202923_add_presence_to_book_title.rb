cf98d564b07ff7a3dad7c79ef5a2187ce5af8c6b
Maria del Carmen Arevalo <marevalofuentes@gmail.com>
2022-11-04 15:50:29 -0700

    Proyecto Final

---
+++ library/db/migrate/20221104202923_add_presence_to_book_title.rb
@@ -0,0 +1,5 @@
+class AddPresenceToBookTitle < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null(:books, :title, false)
+  end
+end
---
+++ library/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(
+    title: 'El Paraíso',
+    category: 'Suspenso',
+    year: 1992
+)
+
+
+Book.create(
+    title: 'El Secreto Oculto',
+    category: 'Misterio',
+    year: 2000
+)
